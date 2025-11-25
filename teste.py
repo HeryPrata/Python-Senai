a825fd1dd1ffc24ca70f71ae175dee68c38576b4
HeryPrata <157115637+HeryPrata@users.noreply.github.com>
2025-11-25 16:20:41 -0300

    Update teste.py

---
+++ teste.py
@@ -0,0 +1,149 @@
+#CADASTRO DO CLIENTE
+cliente = {
+
+"nome": input("Digite o seu nome para iniciar o cadastro na nossa concessioária: "),
+"telefone": input("Digite o seu telefone: "),
+"saldo": float(input("Digite o seu saldo inicial: ")),
+
+}
+
+#Dicionario com os modelos e o valor fipe
+tabela_precos = {
+"F40": 2000000.0,
+"Panamera": 1500000.0,
+"X6": 1000000.0,
+"Sennna": 3000000.0,
+}
+
+#Lista de dicionarios. Com valores atribuidos a objetos.
+# ":" atrela um rotulo nos modelos.
+#rotulos "marca" e "modelo". valores
+carros_aluguel = [
+("Ferrari", "F40"),
+("Mclaren", "Senna"),
+]
+
+carros_vendas = [
+("Ferrari", "F40"),
+("Porche Panamera","Bmw X6")
+("Bmw","X6")
+("Mclaren","Senna")
+
+]
+
+#Copia e cola: veículo, Automóveis, Você, Disponíveis.
+def vender_carro():
+    print("--- VENDA DE VEÍCULOS ---")
+    
+    marca = input;("Digite a marca que você deseja")
+    modelo = input("Digite o modelo da marca que você escolheu")
+    
+    if marca not in tabela_precos:
+        print("A marca não esta presente em nossa lista")
+        return
+    
+    valor_referencia = tabela_precos[modelo]
+    proposta = valor_referencia*0.80 #paga 20% a menos do valor de referência
+    
+    print(f"Valor referencial: {valor_referencia:.2f}")
+    print(f"Prosposta da concessionaria: {proposta:.2f}")
+
+    if cliente["saldo"] < proposta:
+        print("Valor em saldo insuficiente. Tente buscar por outro modelo.")
+        return
+
+    confirmacao = input("Você aceita a prosposta? (s) ou (n): ")
+
+    if confirmacao == "s":
+        cliente ["saldo"] += proposta
+        print("Venda realizada com sucesso!")
+    else:
+        print("Venda cancelada.")
+
+
+def alugar_carro():
+    print("--- ALUGUE DE VEÍCULOS ---")
+    if not carros_aluguel:
+        print("Não a veículo disponivel para aluguel.")
+        return
+    
+    print("\n--- Veículos Disponíveis ---")
+    for carro in carros_aluguel:
+     marca = carro["marca"]
+     modelo = carro["modelo"]
+     print(f"- Marca: {marca}\n- Modelo: {modelo}")
+    
+    escolha = int(input("Escolha o numero correspondente ao carrro do seu interrese: ")) -1 
+    
+    if escolha < 0 or escolha >= len(carros_aluguel):
+        print("Opção invalida.")
+    
+    dias = int(input("Quantos dias você gostaria de alugar o carro?"))
+    
+    valor_final = dias*5
+
+    print(f"O valor total do aluguel é: R$ {valor_final:.2f}")
+
+    print("Verificando seu saldo...")
+
+    if cliente["saldo"] < valor_final:
+        print("Valor insuficiente. Tente buscar outro modelo ou diminuir a quantidade dias.")
+        return
+    
+    confimarcao = input("Saldo suficientel, gostaria de alugar o Automóvel: (s) ou (n)")
+    if confimarcao == "s":
+        cliente["saldo"] -= valor_final
+        print("Transação concluida. Você escolheu: {carro[escolha]}")
+        carro = carros_aluguel.pop(escolha)#O pop é a ação de remover o item com a ação de obter o item em uma única linha.
+    else:
+        print("Transação cancelada.")
+        return
+    
+
+def comprar_carro():
+    print("--- COMPRAR DE VEÍCULOS ---")
+    print("Automóveis disponiveis.")
+
+    for carro in carros_vendas:
+        marca = carros_vendas["marca"]
+        modelo = carros_vendas["modelo"]
+        print(f"Marca - {marca}\n Modelo - {modelo}")    
+
+
+
+
+
+
+
+
+
+
+
+#Menu Principal
+def menu():
+    print("--- SEJA BEM VINDO, A NOSSA CONCESSIONARIA. ---")
+    print("1 - Vender Carro")
+    print("2 - Compra Carro")
+    print("3 - Alugar Carro")
+    print("4 - Ver saldo")
+    print("5 - sair")
+
+while True:
+    menu()
+
+    opcao = input("Escolha um opção: ")
+
+    match opcao:
+        case "1":
+            vender_carro()
+        case "2":
+            comprar_carro()
+        case "3":
+            alugar_carro()
+        case "4":
+            print(f"O saldo disponivel: R${cliente['saldo']:.2f}")
+        case "5":
+            print("Encerrando sistema.")
+            break
+        case _:
+            print("Opção invalida. Tente novamente.")
